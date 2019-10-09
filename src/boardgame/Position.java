e5ec7d7a74a2999e26fb642d2448f8f809bd51d9
moisesmengo <moises.lins@academico.ifrn.edu.br>
2019-10-09 16:57:54 -0300

    first class Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,33 @@
+package boardgame;
+
+public class Position {
+
+    private int row;
+    private int column;
+
+    public Position( int row, int column){
+        this.column = column;
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    @Override
+    public String toString(){
+        return row + ", " + column;
+    }
+}
