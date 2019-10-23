361f4567d1727ed804b096843119774493212765
Moisés Costa Lins <costa.lins@academico.ifrn.edu.br>
2019-10-23 17:07:43 -0300

    board exception and defensive programming

--- src/application/UI.java
+++ src/application/UI.java
@@ -12,7 +12,7 @@ public class UI {
             }
             System.out.println();
         }
-        System.out.println(" a b c d e f g h");
+        System.out.println("  a b c d e f g h");
     }
 
     public static void printPiece(ChessPiece piece){
